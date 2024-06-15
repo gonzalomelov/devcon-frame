76d1032830a076372679759f49aa7860f93fdc0a
Gonzalo Melo <gonzalomelov@gmail.com>
2024-06-15 10:33:42 -0300

    chore: remove unused DemoBadge

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,7 +4,6 @@ import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { NextIntlClientProvider, useMessages } from 'next-intl';
 
-import { DemoBadge } from '@/components/DemoBadge';
 import { AppConfig } from '@/utils/AppConfig';
 
 export const metadata: Metadata = {
@@ -50,8 +49,6 @@ export default function RootLayout(props: {
           messages={messages}
         >
           {props.children}
-
-          <DemoBadge />
         </NextIntlClientProvider>
       </body>
     </html>
